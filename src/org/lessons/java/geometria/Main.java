48c14516d613e9c901cd8d702854bc5497ab882f
DanieleSpuria <123416873+DanieleSpuria@users.noreply.github.com>
2023-09-15 17:02:32 +0200

    Update Main.java

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		
 		for(int i = 1; i <= 3; i++) {
-			System.out.println("\n------Rettangolo " + i + "------\n");
+			System.out.println("\n------Rettangolo " + (i + i) + "------\n");
 			
 			System.out.print("Base: ");
 			int base = scan.nextInt();
@@ -18,11 +18,13 @@ public class Main {
 			int altezza = scan.nextInt();
 			
 			Rettangolo rettangolo = new Rettangolo(base, altezza);
-			int area = rettangolo.area();
+			int area      = rettangolo.area();
 			int perimetro = rettangolo.perimetro();
 			
 			System.out.println("Area: " + area);
 			System.out.println("Perimetro: " + perimetro);
+			rettangolo.disegna();
 		}
+		scan.close();
 	}
 }
