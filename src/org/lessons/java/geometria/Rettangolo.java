9e4b11777f7c3478d60680b53a4d6664317ecdd4
DanieleSpuria <123416873+DanieleSpuria@users.noreply.github.com>
2023-09-15 16:57:53 +0200

    add disegna()

--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -17,4 +17,19 @@ public class Rettangolo {
 	public int perimetro()  {	
 		return (base + altezza) * 2;
 	}
+
+	public void disegna() {
+		for (int i = 1; i <= base; i++) System.out.print("o");
+
+		for (int i = 1; i <= altezza - 2; i++) {
+			System.out.print("\no");
+
+			for (int x = 1; x <= base - 2; x++) System.out.print(' ');
+
+			if (i != altezza -2) System.out.print("o");
+			else System.out.println("o");
+		}
+
+		for (int i = 1; i <= base; i++) System.out.print('o');
+	}
 }
